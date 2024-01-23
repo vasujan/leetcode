dd515ea65cf370c6828d8df599dc10f1b441b0fe
Vasu Jain <vasujain.vj@gmail.com>
2024-01-23 12:38:43 +0530

    15

---
+++ 00_99/15_three_sum/15_three_sum.md
@@ -0,0 +1,11 @@
+
+# 15. Three Sum
+
+[LeetCode - The World's Leading Online Programming Learning Platform](https://leetcode.com/problems/3sum/description/)
+
+## Problem
+
+Given an integer array nums, return all the triplets such that their indices are not the same and their sum is 0.
+
+The solution must not contain duplicate triplets.
+
--- 00_99/15_three_sum/solution.py
+++ 00_99/15_three_sum/solution.py
@@ -1,5 +1,28 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res: list[list[int]] = []
+        nums.sort()
+
+        i = 0
+        while i < len(nums) - 2:
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            a, b = i + 1, len(nums) - 1
+
+            while a < b:
+                _sum = nums[a] + nums[b] + nums[i]
+                if _sum < 0:
+                    a += 1
+                elif _sum > 0:
+                    b -= 1
+                else:
+                    res.append(nums[i], nums[a], nums[b])
+                    a += 1
+                    while nums[a] == nums[a-1] and a < b:
+                        a += 1
+        return res
+
+    def threeSum_0(self, nums: list[int]) -> list[list[int]]:
         bucket = {}
 
         for i in nums:
