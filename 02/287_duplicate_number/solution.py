38aa978e2a1d48aa4bb25991f7a61f3bf1e5a8cf
Vasu Jain <vasujain.vj@gmail.com>
2024-02-16 13:11:07 +0530

    287

--- 02/287_duplicate_number/solution.py
+++ 02/287_duplicate_number/solution.py
@@ -3,9 +3,20 @@ from typing import List
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # without modifying the input array and using constant extra space
-        count = dict()
-        for n in nums:
-            if n in count:
-                return n
-            else:
-                count[n] = 1
+        tortoise = nums[0]
+        hare = nums[0]
+
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        ptr1 = nums[0]
+        ptr2 = tortoise
+
+        while ptr1 != ptr2:
+            ptr1 = nums[ptr1]
+            ptr2 = nums[ptr2]
+        
+        return ptr1
