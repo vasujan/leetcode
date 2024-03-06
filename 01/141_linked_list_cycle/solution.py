a0737e3b3d4e62323f6f7817f011b7bbf51c993a
Vasu Jain <vasujain.vj@gmail.com>
2024-03-06 10:50:45 +0530

    141: slow and fast pointer approach

--- 01/141_linked_list_cycle/solution.py
+++ 01/141_linked_list_cycle/solution.py
@@ -14,3 +14,15 @@ class Solution:
             if current.val is None:
                 return True
         return False
+
+    # Slow and fast pointer
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
