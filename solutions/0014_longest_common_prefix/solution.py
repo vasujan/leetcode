992f5d98c88821d5196d77575c0413a22400be21
Vasu Jain <vasujain.vj@gmail.com>
2024-03-08 15:54:40 +0530

    14

--- solutions/0014_longest_common_prefix/solution.py
+++ solutions/0014_longest_common_prefix/solution.py
@@ -1,4 +1,19 @@
 class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pref = strs[0]
+        
+        for s in strs[1:]:
+            i = 0
+            while i < len(pref) and i < len(s):
+                if pref[i] != s[i]:
+                    break
+                i += 1
+            pref = pref[:i]
+        
+        return pref
+
+
+
     def longestCommonPrefix(self, strs: list[str]) -> str:
         prefix = strs[0]
 
