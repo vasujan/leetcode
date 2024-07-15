50f5b6bcd0cf1c2f5d8f247e268be042bcc6f2dc
Vasu Jain <vasujain.vj@gmail.com>
2024-07-16 05:27:11 +0530

    2196

--- solutions/2196_create_binary_tree/solution.py
+++ solutions/2196_create_binary_tree/solution.py
@@ -5,44 +5,28 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        self._count = None
-    
-    def count(self):
-        if self._count:
-            return self._count
-        count = 0
-        if self.left:
-            count += 1 + self.left.count()
-        if self.right:
-            count += 1 + self.right.count()
-        self._count = count
-        return count
-
-
+        
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         store = {}
 
         for parent, child, left_node in descriptions:
-            if child in store:
-                child = store[child]
-            else: 
-                child = TreeNode(child)
-
             if parent not in store:
-                if left_node:
-                    store[parent] = TreeNode(parent, child, None)
-                else:
-                    store[parent] = TreeNode(parent, None, child)
+                store[parent] = TreeNode(parent)
+            if child not in store:
+                store[child] = TreeNode(child)
+
+            if left_node:
+                store[parent].left = store[child]
             else:
-                if left_node:
-                    store[parent].left = child
-                else:
-                    store[parent].right = child
-                
-        root_node = TreeNode()
-        for t in store.values():
-            if t.count() > root_node.count():
-                root_node = t
-        
-        return root_node
+                store[parent].right = store[child]
+
+
+        root = None
+        children = [item[1] for item in descriptions]
+        for key in store:
+            if key not in children:
+                root = store[key]
+                break
+
+        return roo
